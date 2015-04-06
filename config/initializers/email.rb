dc46336cdc7f4022e3830864fcdb52d6122ad8c2
Ricky Han <rickylqhan@gmail.com>
2015-04-06 19:59:21 -0400

    tree

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,6 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  :address => "127.0.0.1",
-  :port => 25,
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :enable_starttls_auto => true
   :domain => Rails.application.domain,
-  :enable_starttls_auto => false,
 }
