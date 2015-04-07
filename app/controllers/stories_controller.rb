6b2c38418073ed9743333bd6b3f7b5e18d9e8861
Ricky Han <rickylqhan@gmail.com>
2015-04-07 19:50:17 -0400

    imp

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -223,7 +223,7 @@ class StoriesController < ApplicationController
     if @anon
       Vote.vote_thusly_on_story_or_comment_for_user_because(1, story.id,
                                                             nil, 1, nil, true) # user.id = 1 when anon 
-      render :text => 'voted as anon'
+      return render :text => 'voted as anon'
     elsif @user
       Vote.vote_thusly_on_story_or_comment_for_user_because(1, story.id,
         nil, @user.id, nil)
