9d857fae535a92a4acfe88ede0408b7806094da5
Ricky Han <rickylqhan@gmail.com>
2015-04-06 20:19:32 -0400

    tree

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,3 +1,6 @@
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true  
+
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
