4453dc0e788c7c252b41e4579b195c6050b10b42
Ricky Han <rickylqhan@gmail.com>
2015-04-06 21:47:34 -0400

    action mailer

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,7 @@ class << Rails.application
 
   def root_url
     Rails.application.routes.url_helpers.root_url({
-      :host => Rails.application.domain,
+      :host => "www" << Rails.application.domain,
       :protocol => Rails.application.ssl? ? "https" : "http",
     })
   end
