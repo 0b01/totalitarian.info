9673ae8e37fd61c40ab9c8a096688ec71ab9a8f4
Ricky Han <rickylqhan@gmail.com>
2015-04-07 19:37:48 -0400

    imp

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -222,7 +222,7 @@ class StoriesController < ApplicationController
     end
     if @anon
       Vote.vote_thusly_on_story_or_comment_for_user_because(1, story.id,
-                                                            nil, 0, nil) # user.id = 0 when anon 
+                                                            nil, 1, nil) # user.id = 1 when anon 
     elsif @user
       Vote.vote_thusly_on_story_or_comment_for_user_because(1, story.id,
         nil, @user.id, nil)
