03e49bfb60ece7f7e3ba5f26ebba1539ce670487
Ricky Han <rickylqhan@gmail.com>
2015-04-07 22:20:25 -0400

    updated schema, allowing user_id = NULL

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20150313040930) do
     t.datetime "updated_at"
     t.string   "short_id",           limit: 10,                           default: "",    null: false
     t.integer  "story_id",                                                                null: false
-    t.integer  "user_id",                                                                 null: false
+    t.integer  "user_id",
     t.integer  "parent_comment_id"
     t.integer  "thread_id"
     t.text     "comment",                                                                 null: false
@@ -200,7 +200,7 @@ ActiveRecord::Schema.define(version: 20150313040930) do
   add_index "users", ["username"], name: "username", unique: true, using: :btree
 
   create_table "votes", force: true do |t|
-    t.integer "user_id",              null: false
+    t.integer "user_id",
     t.integer "story_id",             null: false
     t.integer "comment_id"
     t.integer "vote",       limit: 2, null: false
